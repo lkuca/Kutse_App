11d20cdf276b1522ee028072b9c200973bfd0e70
opilane <opilane@HP-CZC2349HTQ>
2024-01-17 13:23:39 +0200

    Add project files.

---
+++ Kutse_App/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using Kutse_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace Kutse_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public  ActionResult Kutse()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 10 ? "Tere hommikust" : "Tere päevast";
+            ViewBag.Message = "Ootan sin oma peole! Tule kindlasti!!!ootan sind!";
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Ankeet()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ViewResult Ankeet(Guest guest)
+        {
+
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guest);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public void E_mail(Guest guest)
+        {
+            try
+            {
+                WebMail.SmtpServer = "smtp.gmail.com";
+                WebMail.SmtpPort = 587;
+                WebMail.EnableSsl= true;
+                WebMail.UserName = "klopak@gmail.com";
+                WebMail.Password = "****************";
+                WebMail.From = "klopak@gmail.com";
+                WebMail.Send(guest.Email, "Vastus kutsule",guest.Name + "vastas" +((guest.WillAttend ?? false) ? "tuleb peole" : "ei tule peole"));
+                ViewBag.Message = "Kiri on saatnud!";
+            }
+            catch (Exception)
+            {
+                ViewBag.Message = "Mul on kahju! Ei saa kirja saada!!!";
+            }
+
+        }   
+    }
+}
